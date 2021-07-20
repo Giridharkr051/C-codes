44551e53d2f6e74405dfb8d98a08aac0768f04fc
Giridharkr051 <84076569+Giridharkr051@users.noreply.github.com>
2021-07-20 12:30:23 +0530

    Update inter change large and low no..c

--- inter change large and low no..c
+++ inter change large and low no..c
@@ -1,33 +1,46 @@
 //inter change large and low no.
 #include<stdio.h>
+#include<conio.h>
 void main()
 {
- int a[50],largest,smallest,n,swapper,i;
+	int a[50],n,max,min,maxpos,minpos,i,temp;
+	printf("Enter number of elements(maxlimit 50): ");
+	scanf("%d",&n);
+	printf("\nenter any %d integers:\n",n);
+	for(i=0;i<n;i++)
+		scanf("%d",&a[i]);
+    max=a[0];
+	min=a[0];
+	maxpos=0;
+	minpos=0;
+	for(i=1;i<n;i++)
+	{
+		if(a[i]>max)
+		{
+			max=a[i];
+			maxpos=i;
 
-printf("Enter the number of elements in the array:\n");
-scanf("%d",&n);
+		}
+		if(a[i]<min)
+		{
+			min=a[i];
+			minpos=i;
+		}
+	}
+	temp=a[maxpos];
+	a[maxpos]=a[minpos];
+	a[minpos]=temp;
 
-printf("Enter the elements of the array: \n");
-for(i=0;i<n;i++)
-
- {
-     scanf("%d",&a[i]);}
-
-for(i=1;i<n;i++)
-{
-   { if(a[i]>a[largest])
-    largest=i;}
-
-if(a[i]<a[smallest])
-
-{smallest=i;}
-}
-
- printf("\n Before interchange: \n Largest number = %d \n Smallest number = %d",a[largest],a[smallest]);
-
-swapper=a[largest];
-a[largest]=a[smallest];
-a[smallest]=swapper;
-
-printf("\n After interchange: \n Largest number = %d \n Smallest number = %d",a[largest],a[smallest]);
+	for(i=1;i<n;i++)
+    {
+        printf("\nminimum value is %d at position %d",min,minpos+1);
+        printf("\nmaximum value is %d at position %d",max,maxpos+1);
+        break;
+    }
+    printf("\nAfter interchange array elemnts are: ");
+	for(i=0;i<n;i++)
+		{
+		    printf(" %d ",a[i]);
+        }
+	getch();
 }
